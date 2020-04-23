ed1e76dd32954ae4f8ae917245587015403dc50a
alannahhenry <60479099+alannahhenry@users.noreply.github.com>
2020-04-23 12:31:19 +0100

    Update multiplexer_9to1tb.vhd

--- Assignment-3/Table Files/multiplexer_9to1tb.vhd
+++ Assignment-3/Table Files/multiplexer_9to1tb.vhd
@@ -21,7 +21,7 @@ architecture Behavioral of multiplexer_9to1tb is
 			I7: in std_logic_vector(15 downto 0);
 			I8: in std_logic_vector(15 downto 0);
 			
-			Z: out std_logic_vector(15 downto 0);
+			Z: out std_logic_vector(15 downto 0)
 		);
 	end component;
 	
